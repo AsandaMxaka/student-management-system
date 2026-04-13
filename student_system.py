abac555587ba5180bcf83fdf582563d4e3204e25
Asanda <asandamxaka98@gmail.com>
2026-04-13 15:26:50 +0200

    Add files via upload

--- student_system.py
+++ student_system.py
@@ -1,25 +1,49 @@
-students = []
+import sqlite3
+
+# Connect to database (it will create file automatically)
+conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+# Create table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL
+)
+""")
+
+conn.commit()
+
 
 def add_student():
     name = input("Enter student name: ")
     age = int(input("Enter student age: "))
-    students.append({"name" : name, "age": age})
+
+    cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)", (name, age))
+    conn.commit()
+
     print("Student added successfully!\n")
 
+
 def view_students():
-    if not students:
+    cursor.execute("SELECT * FROM students")
+    rows = cursor.fetchall()
+
+    if not rows:
         print("No students found\n")
     else:
-        for i, student in enumerate(students, start=1):
-            print(f"{i}. Name: {student['name']}, Age: {student['age']}")
-    
+        for row in rows:
+            print(f"{row[0]}. Name: {row[1]}, Age: {row[2]}")
+    print()
+
 
 def menu():
     while True:
-        print("=======STUDENT MANAGEMENT SYSTEM=========")
-        print("1.Add student")
-        print("2.View students")
-        print("3.Exit")
+        print("======= STUDENT MANAGEMENT SYSTEM =======")
+        print("1. Add student")
+        print("2. View students")
+        print("3. Exit")
 
         choice = input("Choose an option: ")
 
@@ -33,4 +57,6 @@ def menu():
         else:
             print("Invalid choice. Try again.\n")
 
-menu()                        
+
+menu()
+conn.close()
